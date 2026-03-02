b887fae31fe0827d381c8911547dd7a8a520a561
Edward Cho <edwardcho1231@gmail.com>
2026-03-02 16:17:31 -0500

    set up clerk FE and set up folder structure in apps/api

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,13 +1,13 @@
-import Express from 'express';
+import express from 'express';
 import cors from 'cors';
+import { apiRouter } from './routes';
 
-const app = Express();
+const app = express();
+
 app.use(cors());
-app.use(Express.json());
+app.use(express.json());
 
-app.get('/health', (req, res) => {
-  res.json({ ok: true });
-});
+app.use('/api/v1', apiRouter);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
